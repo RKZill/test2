fa80c6e0131f510a8cefa5891df6265952bc113d
rkzill <ryankelsey14@outlook.com>
2022-11-10 12:39:56 -0500

    Added author comment

--- hello.rb
+++ hello.rb
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 #hello
 #Author: RKelsey
-=======
-#HI, This is an unwanted but committed change
 
->>>>>>> 51885dd5833a0780ff5cd53c3ddfa823c67308ac
 name = ARGV.first || "World"
 
 puts "HI, #{name}!"
